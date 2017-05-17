dd526f4151b3856063e447ce5c2c019d01cc4151
JaeinCho <jaein1019@naver.com>
2017-05-18 00:03:54 +0900

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,59 +1,196 @@
-package com.example.chocho64.clanner;
+package com.example.chocho64.mapiary;
 
-import android.app.TabActivity;
 import android.content.Intent;
-import android.graphics.Color;
+import android.net.Uri;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.widget.TabHost;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
-public class MainActivity extends TabActivity {
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+    private ViewPager mViewPager;
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
-        final TabHost tabHost = getTabHost();
-        TabHost.TabSpec spec;
-        Intent intent;
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowTitleEnabled(false);
 
-        tabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+        mViewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
-            public void onTabChanged(String tabId) {
-                for (int i = 0; i < tabHost.getTabWidget().getChildCount(); i++) {
-                    TextView textView = (TextView) tabHost.getTabWidget().getChildAt(i).findViewById(android.R.id.title);
-                    textView.setTextColor(Color.parseColor("#aaaaaa"));
-                    textView.setGravity(Gravity.CENTER);
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                TextView title = (TextView) findViewById(R.id.titleText);
+                switch (position) {
+                    case 0:
+                        title.setText("일기");
+                        break;
+                    case 1:
+                        title.setText("일정");
+                        break;
+                    case 2:
+                        title.setText("장소");
+                        break;
+                    case 3:
+                        title.setText("기타");
+                        break;
                 }
-                TextView textView = (TextView) tabHost.getTabWidget().getChildAt(tabHost.getCurrentTab()).findViewById(android.R.id.title);
-                textView.setTextColor(Color.parseColor("#FFFFFF"));
-                textView.setGravity(Gravity.CENTER);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
             }
         });
 
-        intent = new Intent(this, TableActivity.class);
-        spec = tabHost.newTabSpec("tab1").setIndicator("시간표").setContent(intent);
-        tabHost.addTab(spec);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
 
-        intent = new Intent(this, TimeActivity.class);
-        spec = tabHost.newTabSpec("tab2").setIndicator("수업중").setContent(intent);
-        tabHost.addTab(spec);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
-        intent = new Intent(this, DateActivity.class);
-        spec = tabHost.newTabSpec("tab3").setIndicator("알림").setContent(intent);
-        tabHost.addTab(spec);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
 
-        for (int i = 0; i < tabHost.getTabWidget().getChildCount(); i++) {
-            TextView textView = (TextView) tabHost.getTabWidget().getChildAt(i).findViewById(android.R.id.title);
-            textView.setTextColor(Color.parseColor("#aaaaaa"));
-            textView.setGravity(Gravity.CENTER);
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, MapsActivity.class);
+            startActivity(intent);
+            return true;
         }
-        TextView textView = (TextView) tabHost.getTabWidget().getChildAt(tabHost.getCurrentTab()).findViewById(android.R.id.title);
-        textView.setTextColor(Color.parseColor("#FFFFFF"));
-        textView.setGravity(Gravity.CENTER);
 
-        tabHost.setCurrentTab(0);
+        return super.onOptionsItemSelected(item);
+    }
+
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page")
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
+
+    public static class PlaceholderFragment extends Fragment {
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public PlaceholderFragment() {
+
+        }
+
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            return rootView;
+        }
+    }
+
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return 4;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "일기";
+                case 1:
+                    return "일정";
+                case 2:
+                    return "장소";
+                case 3:
+                    return "기타";
+            }
+            return null;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            Fragment fragment = null;
+            switch (position) {
+                case 0:
+                    fragment = new Diary();
+                    break;
+                case 1:
+                    fragment = new Date();
+                    break;
+                case 2:
+                    fragment = new Place();
+                    break;
+                case 3:
+                    fragment = new Etc();
+                    break;
+            }
+            return fragment;
+        }
     }
 }
--- MapsActivity.java
+++ MapsActivity.java
@@ -1,27 +1,17 @@
-package com.example.chocho64.map;
+package com.example.chocho64.mapiary;
 
-import android.app.AlertDialog;
-import android.content.pm.PackageManager;
-import android.location.Address;
-import android.location.Geocoder;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.FragmentActivity;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.io.IOException;
-import java.util.List;
-
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
@@ -29,46 +19,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
-    public boolean onMarkerDrag(Marker marker) {
-        EditText subject = new EditText(this);
-
-        AlertDialog.Builder alert = new AlertDialog.Builder(this);
-        alert.setMessage("메모를 남겨주세요.");
-        alert.setView(subject);
-        return false;
-    }
-
-    public void onMapSearch(View v) {
-        EditText locationSearch = (EditText) findViewById(R.id.editText);
-        final String location = locationSearch.getText().toString();
-        List<Address> addressList = null;
-
-        if (location != null || !location.equals("")) {
-            Geocoder geocoder = new Geocoder(this);
-            try {
-                addressList = geocoder.getFromLocationName(location, 1);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            Address address = addressList.get(0);
-            LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
-            mMap.addMarker(new MarkerOptions().position(latLng).title(location));
-            mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
-        }
-    }
-
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(final GoogleMap googleMap) {
         mMap = googleMap;
-        final LatLng position = new LatLng(37.487634, 126.919113);
+        LatLng position = new LatLng(37.56, 126.97);
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(position, 15));
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
-        mMap.setMyLocationEnabled(true);
     }
 }
